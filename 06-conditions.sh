651332f16ade706f71f435fc3a6798b014f754fa
Vamsi Guna <vamsi.g@sumukha.tech>
2025-04-05 18:52:35 +0530

    added 6th script

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -4,6 +4,6 @@ n1=$1
 
 if [$n1 -gt 100]
 then
-echo "Give number $n1 is greater than 100"
+   echo "Give number $n1 is greater than 100"
 else
-echo "Give number $n1 is less than 100"
+   echo "Give number $n1 is less than 100"
